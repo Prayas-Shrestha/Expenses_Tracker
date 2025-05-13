af1c7e43424974433563101ebad2cb3f2fabf429
Prayas-Shrestha <np03cs4a230356@heraldcollege.edu.np>
2025-05-13 20:15:50 +0545

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,22 +1,22 @@
 const jwt = require("jsonwebtoken");
 
+// Middleware to protect routes with JWT authentication
 module.exports = (req, res, next) => {
   const authHeader = req.header("Authorization");
 
+  //Check if token exists
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   try {
-    const token = authHeader.split(" ")[1];  // Extract token from header
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);  // Decode the token
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = { _id: decoded.user.id };  // Attach user ID to request object
-    console.log("✅ Auth Middleware set req.user:", req.user);  // Debug log to see if it's set correctly
-
-    next();  // Proceed to the next middleware or route handler
-  } catch (err) {
-    console.error("❌ Error in JWT verification:", err);  // Log JWT errors
+    // Attach user ID from token to request
+    req.user = decoded.user.id;
+    next();
+  } catch (error) {
     return res.status(401).json({ msg: "Invalid token" });
   }
 };
